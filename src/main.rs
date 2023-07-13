0aca9cc3fffa5a404d700f863008940928fcd35f
hicaru <lich666black@gmail.com>
2023-07-13 11:42:11 +0500

    added router

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
-    });
+    // let ui_handle = ui.as_weak();
+    // ui.on_request_increase_value(move || {
+    //     let ui = ui_handle.unwrap();
+    //     ui.set_counter(ui.get_counter() + 1);
+    // });
 
     ui.run()
 }
-
