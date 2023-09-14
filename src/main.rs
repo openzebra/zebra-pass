27cb1b3d22b27f38969da6dc73c8a28bdf98c944
Hicaru <hicc0rus@gmail.com>
2023-09-14 20:02:59 +0400

    added translate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+    slint::init_translations!(concat!(env!("CARGO_MANIFEST_DIR"), "/lang/"));
+
+    let app = AppWindow::new()?;
 
     // let ui_handle = ui.as_weak();
     // ui.on_request_increase_value(move || {
@@ -9,5 +11,5 @@ fn main() -> Result<(), slint::PlatformError> {
     //     ui.set_counter(ui.get_counter() + 1);
     // });
 
-    ui.run()
+    app.run()
 }
