67a204f4dca493f2ee7749db4b63225b8fa7398c
Hicaru <hicc0rus@gmail.com>
2023-09-27 16:50:01 +0400

    added copyrights

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// -- Copyright (c) 2023 Rina Khasanshin
+// -- Email: hicarus@yandex.ru
+// -- Licensed under the GNU General Public License Version 3.0 (GPL-3.0)
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
--- src/storage/keys.rs
+++ src/storage/keys.rs
@@ -1,1 +1,5 @@
+// -- Copyright (c) 2023 Rina Khasanshin
+// -- Email: hicarus@yandex.ru
+// -- Licensed under the GNU General Public License Version 3.0 (GPL-3.0)
+
 pub const SLED_DATA_KEY: &str = "ZEBRA_PASS_KEY";
