9f8a92d560ab787e660f22999d213c74f4d5e385
hicaru <lich666black@gmail.com>
2023-09-27 19:43:26 +0500

    added get by key from storage

--- src/storage/db.rs
+++ src/storage/db.rs
@@ -4,17 +4,15 @@
 
 use crate::storage::errors::StorageErrors;
 use directories::ProjectDirs;
-use sled;
 use sled::Db;
 
 pub struct LocalStorage {
-    path: ProjectDirs,
     tree: Db,
 }
 
 impl LocalStorage {
     pub fn new() -> Result<Self, StorageErrors> {
-        let path = match ProjectDirs::from("com", "Zebra Corp", "Zebra App") {
+        let path = match ProjectDirs::from("com.zebra", "Zebra Corp", "Zebra App") {
             Some(p) => p,
             None => return Err(StorageErrors::PathError),
         };
@@ -23,15 +21,38 @@ impl LocalStorage {
             Err(e) => return Err(StorageErrors::StorageAccess(e.to_string())),
         };
 
-        Ok(LocalStorage { path, tree })
+        Ok(LocalStorage { tree })
+    }
+
+    pub fn set(&self, key: &str) {
+        // self.tree.set(&"key");
+    }
+
+    pub fn get(&self, key: &str) -> Result<Option<Vec<u8>>, StorageErrors> {
+        let value = match self.tree.get(key) {
+            Ok(some_value) => {
+                if let Some(value) = some_value {
+                    value.to_vec()
+                } else {
+                    return Ok(None);
+                }
+            }
+            Err(e) => return Err(StorageErrors::StorageRead(e.to_string())),
+        };
+
+        Ok(Some(value.to_vec()))
     }
 }
 
-#[test]
-fn test_dir_detect() {
-    // if let Some(proj_dirs) = ProjectDirs::from("com", "Zebra Corp", "Zebra App") {
-    //     let path = proj_dirs.config_dir();
-    //     println!("{:?}", path);
-    //     let tree = sled::open(path);
-    // }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_local_storage_init() {
+        LocalStorage::new().unwrap();
+    }
+
+    #[test]
+    fn test_set_read() {}
 }
--- src/storage/errors.rs
+++ src/storage/errors.rs
@@ -2,7 +2,10 @@
 //! -- Email: hicarus@yandex.ru
 //! -- Licensed under the GNU General Public License Version 3.0 (GPL-3.0)
 
+#[derive(Debug)]
 pub enum StorageErrors {
     PathError,
     StorageAccess(String),
+    StorageRead(String),
+    StorageReadParseJson(String),
 }
