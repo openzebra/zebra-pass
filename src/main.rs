2f37b15f4911840ec05118e2e6240eb321948e86
Hicaru <hicc0rus@gmail.com>
2023-10-08 13:36:11 +0500

    added layout for Create page

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,23 @@
 // -- Email: hicarus@yandex.ru
 // -- Licensed under the GNU General Public License Version 3.0 (GPL-3.0)
 
-// slint::include_modules!();
+slint::include_modules!();
 
-fn main() {
-    unimplemented!();
-}
-
-// fn main() -> Result<(), slint::PlatformError> {
-//     slint::init_translations!(concat!(env!("CARGO_MANIFEST_DIR"), "/locale/"));
-//
-//     let app = AppWindow::new()?;
-//
-//     let ui_handle = app.as_weak();
-//
-//     // ui.on_request_increase_value(move || {
-//     //     // let ui = ui_handle.unwrap();
-//     //     // ui.set_counter(ui.get_counter() + 1);
-//     // });
-//
-//     app.run()
+// fn main() {
+//     unimplemented!();
 // }
+
+fn main() -> Result<(), slint::PlatformError> {
+    slint::init_translations!(concat!(env!("CARGO_MANIFEST_DIR"), "/locale/"));
+
+    let app = AppWindow::new()?;
+
+    let ui_handle = app.as_weak();
+
+    // ui.on_request_increase_value(move || {
+    //     // let ui = ui_handle.unwrap();
+    //     // ui.set_counter(ui.get_counter() + 1);
+    // });
+
+    app.run()
+}
