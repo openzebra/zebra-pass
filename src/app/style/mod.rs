f0f6a13159c6e49c482e7bd28f45a54b2bee30af
Hicaru <hicc0rus@gmail.com>
2023-11-18 11:26:49 +0500

    added workspace zebra-lib

--- src/app/style/mod.rs
+++
@@ -1,6 +0,0 @@
-//! -- Copyright (c) 2023 Rina Khasanshin
-//! -- Email: hicarus@yandex.ru
-//! -- Licensed under the GNU General Public License Version 3.0 (GPL-3.0)
-
-pub mod button;
-pub mod list_group;
--- zebra-lib/src/lib.rs
+++ zebra-lib/src/lib.rs
@@ -1,11 +1,6 @@
 // -- Copyright (c) 2023 Rina Khasanshin
 // -- Email: hicarus@yandex.ru
 // -- Licensed under the GNU General Public License Version 3.0 (GPL-3.0)
-extern crate rust_i18n;
-
-use rust_i18n::i18n;
-
-pub mod app;
 pub mod bip39;
 pub mod config;
 pub mod core;
@@ -16,5 +11,3 @@ pub mod records;
 pub mod settings;
 pub mod state;
 pub mod storage;
-
-i18n!("locales", fallback = "en");
