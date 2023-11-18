372669035d4c5ea7068068f0c5e61127f34b1d7a
Hicaru <hicc0rus@gmail.com>
2023-11-18 12:41:54 +0500

    added workspace zebra-ui

--- zebra-ui/src/color.rs
+++ zebra-ui/src/color.rs
@@ -1,13 +1,4 @@
 //! -- Copyright (c) 2023 Rina Khasanshin
 //! -- Email: hicarus@yandex.ru
 //! -- Licensed under the GNU General Public License Version 3.0 (GPL-3.0)
-extern crate rust_i18n;
-
-use rust_i18n::i18n;
-use zebra_ui::theme;
-
-i18n!("locales", fallback = "en");
-
-fn main() {
-    println!("Hello, world!");
-}
+use iced::Color;
