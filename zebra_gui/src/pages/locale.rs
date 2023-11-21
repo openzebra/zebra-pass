7349aee2f6d65c5b9b464c25ee873aaf74e8b6dd
Hicaru <hicc0rus@gmail.com>
2023-11-21 13:46:51 +0500

    added picklist

--- zebra_gui/src/pages/locale.rs
+++ zebra_gui/src/pages/locale.rs
@@ -2,28 +2,33 @@
 //! -- Email: hicarus@yandex.ru
 //! -- Licensed under the GNU General Public License Version 3.0 (GPL-3.0)
 use crate::rust_i18n::t;
-use iced::{alignment::Horizontal, Alignment, Command, Length, Subscription};
+use iced::{
+    alignment::Horizontal,
+    widget::{combo_box, pick_list, ComboBox},
+    Alignment, Command, Length, Subscription,
+};
 use zebra_ui::widget::*;
 
 #[derive(Debug)]
 pub struct Locale {
-    available_locales: Vec<String>,
+    locales: Vec<String>,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum LocaleMessage {
     Chose,
     Next,
+    Selected(String),
 }
 
 impl Locale {
     pub fn new() -> Self {
-        let available_locales = rust_i18n::available_locales!()
+        let locales = rust_i18n::available_locales!()
             .iter()
             .map(|s| String::from(*s))
             .collect::<Vec<String>>();
 
-        Self { available_locales }
+        Self { locales }
     }
 
     pub fn subscription(&self) -> Subscription<LocaleMessage> {
@@ -35,6 +40,14 @@ impl Locale {
     }
 
     pub fn view(&self) -> Element<LocaleMessage> {
+        let locale_pick_list: iced::widget::PickList<'_, String, LocaleMessage, Renderer> =
+            pick_list(
+                &self.locales,
+                Some("en".to_string()),
+                LocaleMessage::Selected,
+            )
+            .style(zebra_ui::theme::PickList::Primary);
+
         let zebra_print = zebra_ui::image::zebra_print_view();
         let title = Text::new(t!("welcome"))
             .size(24)
@@ -58,6 +71,7 @@ impl Locale {
             .align_items(iced::Alignment::Center)
             .padding(50)
             .push(title)
+            .push(locale_pick_list)
             .push(btn);
         let row = Row::new()
             .width(Length::Fill)
