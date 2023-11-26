94d42868a67217487f2c4618af67eccd327fd977
Hicaru <hicc0rus@gmail.com>
2023-11-26 15:21:56 +0500

    added zebra, atom svgs

--- zebra_ui/src/image.rs
+++ zebra_ui/src/image.rs
@@ -7,6 +7,8 @@ use crate::{style::svg, widget::Svg};
 use iced::{widget::svg::Handle, window::icon};
 
 const PRINT: &[u8] = include_bytes!("../static/imgs/zebra_print.svg");
+const ZEBRAS_HEAT: &[u8] = include_bytes!("../static/imgs/zebra.svg");
+const ATOM: &[u8] = include_bytes!("../static/imgs/atom.svg");
 const APP_ICON: &[u8] = include_bytes!("../static/imgs/logo.webp");
 
 pub fn zebra_print_view() -> Svg {
@@ -20,4 +22,22 @@ pub fn zebra_print_view() -> Svg {
 
 pub fn zebra_app_icon() -> icon::Icon {
     icon::from_file_data(APP_ICON, None).unwrap()
+}
+
+pub fn zebra_heat() -> Svg {
+    let h = Handle::from_memory(ZEBRAS_HEAT);
+    Svg::new(h)
+        .height(Length::Fill)
+        .height(Length::Fill)
+        .content_fit(ContentFit::Cover)
+        .style(svg::Svg::Inverse)
+}
+
+pub fn atom() -> Svg {
+    let h = Handle::from_memory(ATOM);
+    Svg::new(h)
+        .height(Length::Fill)
+        .height(Length::Fill)
+        .content_fit(ContentFit::Cover)
+        .style(svg::Svg::Inverse)
 }
