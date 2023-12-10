d136fc38c4217a37b17bc5f34966483d141e8bb1
Hicaru <hicc0rus@gmail.com>
2023-12-10 17:46:20 +0700

    added rerender words;

--- zebra_gui/src/pages/gen_phrase.rs
+++ zebra_gui/src/pages/gen_phrase.rs
@@ -3,26 +3,48 @@
 //! -- Licensed under the GNU General Public License Version 3.0 (GPL-3.0)
 use iced::{Command, Length, Subscription};
 use std::sync::{Arc, Mutex};
-use zebra_lib::{core::core::Core, errors::ZebraErrors};
+use zebra_lib::{bip39::mnemonic::Mnemonic, core::core::Core, errors::ZebraErrors};
 
 use crate::gui::GlobalMessage;
+use rand;
 
 use super::Page;
 use zebra_ui::widget::*;
 
 #[derive(Debug)]
 pub struct GenPhrase {
+    pub words: Vec<String>,
+    pub count: usize,
+    pub error_msg: Option<String>,
     core: Arc<Mutex<Core>>,
 }
 
 #[derive(Debug)]
-pub enum GenPhraseMessage {}
+pub enum GenPhraseMessage {
+    ReGenerate,
+}
 
 impl Page for GenPhrase {
     type Message = GenPhraseMessage;
 
     fn new(core: Arc<Mutex<Core>>) -> Result<Self, ZebraErrors> {
-        Ok(Self { core })
+        let mut rng = rand::thread_rng();
+        let count = 12; // number of words
+        let m = Mnemonic::gen(
+            &mut rng,
+            count,
+            zebra_lib::bip39::mnemonic::Language::English,
+        )
+        .or(Err(ZebraErrors::Bip39InvalidMnemonic))?;
+        let words = m.get_vec().iter().map(|s| s.to_string()).collect();
+        let error_msg = None;
+
+        Ok(Self {
+            core,
+            words,
+            error_msg,
+            count,
+        })
     }
 
     fn subscription(&self) -> iced::Subscription<Self::Message> {
@@ -30,7 +52,27 @@ impl Page for GenPhrase {
     }
 
     fn update(&mut self, message: Self::Message) -> Command<GlobalMessage> {
-        Command::none()
+        match message {
+            GenPhraseMessage::ReGenerate => {
+                let mut rng = rand::thread_rng();
+                let m = Mnemonic::gen(
+                    &mut rng,
+                    self.count,
+                    zebra_lib::bip39::mnemonic::Language::English,
+                );
+
+                match m {
+                    Ok(m) => {
+                        self.words = m.get_vec().iter().map(|s| s.to_string()).collect();
+                        Command::none()
+                    }
+                    Err(e) => {
+                        self.error_msg = Some(e.to_string());
+                        Command::none()
+                    }
+                }
+            }
+        }
     }
 
     fn view(&self) -> Element<Self::Message> {
