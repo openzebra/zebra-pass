516cb913e239e814f8b984aa185d8a315448be07
Hicaru <hicc0rus@gmail.com>
2023-12-20 16:04:54 +0700

    added layout checkboxies

--- zebra_gui/src/pages/password_setup.rs
+++ zebra_gui/src/pages/password_setup.rs
@@ -17,6 +17,8 @@ use std::sync::{Arc, Mutex};
 use zebra_lib::{bip39::mnemonic::Mnemonic, core::core::Core, errors::ZebraErrors};
 use zebra_ui::widget::*;
 
+pub const MIN_PASSWORD_SIZE: usize = 6;
+
 #[derive(Debug)]
 pub enum LastRoute {
     Gen,
@@ -28,7 +30,10 @@ pub struct PasswordSetup {
     pub last_route: LastRoute,
     password: String,
     confirm_password: String,
+    email: String,
     approved: bool,
+    server_sync: bool,
+    email_restore: bool,
     core: Arc<Mutex<Core>>,
     mnemonic: Option<Mnemonic>,
 }
@@ -38,8 +43,11 @@ pub enum PasswordSetupMessage {
     Next,
     Back,
     ApprovePolicy(bool),
+    ApproveServerSync(bool),
+    ApproveEmailRestore(bool),
     OnPasswordInputed(String),
     OnConfirmPasswordInputed(String),
+    OnEmailInputed(String),
 }
 
 impl Page for PasswordSetup {
@@ -51,9 +59,15 @@ impl Page for PasswordSetup {
         let password = String::new();
         let confirm_password = String::new();
         let approved = false;
+        let server_sync = true;
+        let email_restore = true;
+        let email = String::new();
 
         Ok(Self {
+            email,
             core,
+            email_restore,
+            server_sync,
             approved,
             mnemonic,
             last_route,
@@ -97,6 +111,23 @@ impl Page for PasswordSetup {
                 self.confirm_password = v;
                 Command::none()
             }
+            PasswordSetupMessage::OnEmailInputed(v) => {
+                self.email = v;
+                Command::none()
+            }
+            PasswordSetupMessage::ApproveServerSync(v) => {
+                self.server_sync = v;
+                Command::none()
+            }
+            PasswordSetupMessage::ApproveEmailRestore(v) => {
+                self.email_restore = v;
+
+                if !v {
+                    self.email = String::new();
+                }
+
+                Command::none()
+            }
         }
     }
 
@@ -109,10 +140,13 @@ impl Page for PasswordSetup {
         let title = Text::new(t!("setup_account_and_password"))
             .size(34)
             .horizontal_alignment(Horizontal::Center);
-        let forward_icon = zebra_ui::image::forward_icon()
-            .height(50)
-            .width(50)
-            .style(zebra_ui::style::svg::Svg::Primary);
+        let forward_icon = zebra_ui::image::forward_icon().height(50).width(50).style(
+            if self.approved && self.password == self.confirm_password {
+                zebra_ui::style::svg::Svg::Primary
+            } else {
+                zebra_ui::style::svg::Svg::PrimaryDisabled
+            },
+        );
         let back_btn = Button::new(zebra_ui::image::back_icon().height(50).width(50))
             .padding(0)
             .style(zebra_ui::style::button::Button::Transparent)
@@ -120,7 +154,11 @@ impl Page for PasswordSetup {
         let forward_btn = Button::new(forward_icon)
             .padding(0)
             .style(zebra_ui::style::button::Button::Transparent)
-            .on_press(PasswordSetupMessage::Next);
+            .on_press_maybe(if self.approved && self.password == self.confirm_password {
+                Some(PasswordSetupMessage::Next)
+            } else {
+                None
+            });
         let btns_row = Row::new().push(back_btn).push(forward_btn);
         let content_col = Column::new()
             .height(Length::Fill)
@@ -159,11 +197,47 @@ impl PasswordSetup {
     }
 
     pub fn view_info<'a>(&self) -> Container<'a, PasswordSetupMessage> {
+        let title = Text::new(t!("zebra_futures"))
+            .size(14)
+            .width(Length::Fill)
+            .horizontal_alignment(Horizontal::Left);
+        let server_sync_check_box = Checkbox::new(
+            t!("server_sync_check_box"),
+            self.server_sync,
+            PasswordSetupMessage::ApproveServerSync,
+        );
+        let server_sync_row = Row::new()
+            .push(server_sync_check_box)
+            .width(Length::Fill)
+            .align_items(iced::Alignment::Start);
+        let email_restore_check_box = Checkbox::new(
+            t!("server_sync_check_box"),
+            self.email_restore,
+            PasswordSetupMessage::ApproveEmailRestore,
+        );
+        let email_restore_row = Row::new()
+            .push(email_restore_check_box)
+            .width(Length::Fill)
+            .align_items(iced::Alignment::Start);
+        let mut email_input = text_input(&t!("placeholder_email"), &self.email)
+            .size(14)
+            .width(250)
+            .style(zebra_ui::style::text_input::TextInput::Primary);
+
+        if self.email_restore {
+            email_input = email_input.on_input(PasswordSetupMessage::OnEmailInputed);
+        }
+
         let options_col = Column::new()
             .align_items(iced::Alignment::Center)
-            .padding(20)
+            .padding(10)
+            .spacing(5)
             .height(Length::Fill)
-            .width(Length::Fill);
+            .width(Length::Fill)
+            .push(title)
+            .push(server_sync_row)
+            .push(email_restore_row)
+            .push(email_input);
         Container::new(options_col)
             .height(152)
             .width(350)
@@ -172,15 +246,18 @@ impl PasswordSetup {
 
     pub fn view_content<'a>(&self, m: &Mnemonic) -> Container<'a, PasswordSetupMessage> {
         let info = self.view_info();
+        let error_msg = Text::new("error")
+            .style(zebra_ui::style::text::Text::Dabger)
+            .size(14);
         let passowrd = text_input(&t!("placeholder_password"), &self.password)
-            .size(14)
+            .size(20)
             .width(250)
             .password()
             .on_input(PasswordSetupMessage::OnPasswordInputed)
             .style(zebra_ui::style::text_input::TextInput::Primary);
         let confirm_passowrd =
             text_input(&t!("placeholder_confirm_password"), &self.confirm_password)
-                .size(14)
+                .size(20)
                 .width(250)
                 .password()
                 .on_input(PasswordSetupMessage::OnConfirmPasswordInputed)
@@ -194,14 +271,20 @@ impl PasswordSetup {
             self.approved,
             PasswordSetupMessage::ApprovePolicy,
         );
+        let chec_row = Row::new()
+            .push(check_box)
+            .width(250)
+            .align_items(iced::Alignment::Start);
         let main_col = Column::new()
             .align_items(iced::Alignment::Center)
             .push(Space::new(0, 20))
             .push(info)
-            .push(Space::new(0, 20))
+            .push(Space::new(0, 5))
+            .push(error_msg)
+            .push(Space::new(0, 5))
             .push(in_col)
-            .push(Space::new(0, 20))
-            .push(check_box);
+            .push(Space::new(0, 10))
+            .push(chec_row);
 
         Container::new(main_col)
     }
