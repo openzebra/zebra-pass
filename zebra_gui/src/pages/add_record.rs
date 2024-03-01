9d2be65f259237403b3b32404a4a198e33f4afe5
hicaru <lich666black@gmail.com>
2024-03-01 09:58:40 +0500

    added smaple smart-input

--- zebra_gui/src/pages/add_record.rs
+++ zebra_gui/src/pages/add_record.rs
@@ -105,17 +105,27 @@ impl Page for AddRecordPage {
 
 impl AddRecordPage {
     pub fn input(&self) -> Container<AddRecordPageMessage> {
-        let label = Text::new("name").size(13);
+        let label = Text::new("name").size(12);
         let input = text_input("", "")
-            .size(16)
-            .width(250)
-            .padding(8)
+            .size(14)
+            .padding(4)
             .secure(true)
             .on_input(AddRecordPageMessage::HanldeInputName)
             .style(zebra_ui::style::text_input::TextInput::Transparent);
         let col = Column::new().push(label).push(input);
+        let copy_btn = Button::new(zebra_ui::image::copy_icon().height(25).width(25))
+            .padding(0)
+            .style(zebra_ui::style::button::Button::Transparent);
+        let reload_btn = Button::new(zebra_ui::image::reload_icon().height(30).width(30))
+            .padding(0)
+            .style(zebra_ui::style::button::Button::Transparent);
+        let row = Row::new()
+            .align_items(iced::Alignment::Center)
+            .push(col)
+            .push(copy_btn)
+            .push(reload_btn);
 
-        Container::new(col)
+        Container::new(row)
             .padding(3)
             .style(zebra_ui::style::container::Container::SecondaryRoundedBox)
     }
